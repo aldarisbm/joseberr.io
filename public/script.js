0f2464ff981da67533a37a4e1bdfcc8d35c10cf0
Jose Berrio <aldarisbm@gmail.com>
2019-09-28 23:44:02 -0500

    cowboying it

--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
 })
 let intro = document.getElementById('intro')
+let githubLink = document.getElementById('github')
+let linkedinLink = document.getElementById('linkedin')
 let introString = 'Software Developer / Cloud Engineer'
+let github = '<a href="https://www.github.com/aldarisbm">GitHub</a>'
+let linkedin = '<a href="https://www.linkedin.com/in/aldarisbm/">LinkedIn</a>' 
 let lengthOfIntro = introString.length
 let i = 0
 let repeatFlag = true
@@ -11,6 +15,12 @@ function printIntro(){
     intro.innerHTML = intro.innerHTML + introString.charAt(i)
     i++
     if (i == lengthOfIntro) {
+      setTimeout(()=>{
+        githubLink.innerHTML = github
+      }, 1000)
+      setTimeout(()=> {
+        linkedinLink.innerHTML = linkedin 
+      }, 1750)
       clearInterval(intervalId)
       setInterval(printIntro, 500)
     }
