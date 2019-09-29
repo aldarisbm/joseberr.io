f2d433c4d6e2ff091737edf6493cc57f7f72439a
Jose Berrio <aldarisbm@gmail.com>
2019-09-29 00:39:28 -0500

    refactoring

--- public/script.js
+++ public/script.js
@@ -1,34 +1,38 @@
 document.addEventListener('DOMContentLoaded', function(){
-})
-let intro = document.getElementById('intro')
-let githubLink = document.getElementById('github')
-let linkedinLink = document.getElementById('linkedin')
-let introString = 'Software Developer / Cloud Engineer'
-let github = '<a href="https://www.github.com/aldarisbm">GitHub</a>'
-let linkedin = '<a href="https://www.linkedin.com/in/aldarisbm/">LinkedIn</a>' 
-let lengthOfIntro = introString.length
-let i = 0
-let repeatFlag = true
-let intervalId = setInterval(printIntro, 150)
-function printIntro(){
-  if (i < lengthOfIntro) {
-    intro.innerHTML = intro.innerHTML + introString.charAt(i)
-    i++
-    if (i == lengthOfIntro) {
-      setTimeout(()=>{
-        githubLink.innerHTML = github
-      }, 1000)
-      setTimeout(()=> {
-        linkedinLink.innerHTML = linkedin 
-      }, 1750)
-      clearInterval(intervalId)
-      setInterval(printIntro, 500)
+
+  let intro = document.getElementById('intro')
+  let github = document.getElementById('github')
+  let linkedin = document.getElementById('linkedin')
+
+  let introString = 'Software Developer / Cloud Engineer'
+  let githubLink = '<a href="https://www.github.com/aldarisbm">GitHub</a>'
+  let linkedinLink = '<a href="https://www.linkedin.com/in/aldarisbm/">LinkedIn</a>' 
+
+  let introLength = introString.length
+  let i = 0
+  let tick = true
+  let intervalId = setInterval(printIntro, 150)
+
+  function printIntro(){
+    if (i < introLength) {
+      intro.innerHTML = intro.innerHTML + introString.charAt(i)
+      i++
+      if (i == introLength) {
+        setTimeout(() => {
+          github.innerHTML = githubLink
+        }, 1000)
+        setTimeout(() => {
+          linkedin.innerHTML = linkedinLink
+        }, 1750)
+        clearInterval(intervalId)
+        setInterval(printIntro, 500)
+      }
+    } else if (i == introLength && tick) {
+      intro.innerHTML = introString + '_'
+      tick = false
+    } else {
+      intro.innerHTML = introString + '&nbsp;&nbsp;'
+      tick = true
     }
-  } else if (i == lengthOfIntro && repeatFlag) {
-    intro.innerHTML = introString + '_'
-    repeatFlag = false
-  } else {
-    intro.innerHTML = introString + '&nbsp;&nbsp;'
-    repeatFlag = true
   }
-}
+})
